cb7a3729a6be219140d8b606ddde8cdfc3e4aeef
Nami-Doc <vendethiel@hotmail.fr>
2012-06-12 18:46:10 +0200

    Use composer

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-require 'asset.php';
 function vardump(){echo'<pre>';$e=func_get_args();foreach($e as $a)var_dump($a);}
 function vdump(){call_user_func_array('vardump',func_get_args());exit;}
 function camelize($s){$s=str_replace('_',' ',$s);$s=ucwords($s);return str_replace(' ','',$s);}
 function pascalize($s){return lcfirst(camelize($s));}
+require 'vendor/autoload.php';
 
 
 $pipeline = new Asset\Pipeline(glob('assets/*'));
