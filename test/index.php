1c727fef94b5632334315380c927298f221967ce
Nami-Doc <vendethiel@hotmail.fr>
2014-06-01 19:08:26 +0200

    Easier to use & harder to break tests

--- test/index.php
+++ test/index.php
@@ -1,12 +1,12 @@
 <?php
 function vardump(){echo'<pre>';$e=func_get_args();foreach($e as $a)var_dump($a);}
 function vdump(){call_user_func_array('vardump',func_get_args());exit;}
-require '../vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 
 // read paths.json - see below
 // you can of course pass a normal array !
-$paths = str_replace('%template%', 'MyTemplate', file_get_contents('paths.json'));
+$paths = str_replace('%template%', 'MyTemplate', file_get_contents(__DIR__.'/paths.json'));
 $paths = json_decode($paths, true);
 
 // create a pipeline
@@ -17,10 +17,31 @@ $css = new Sprockets\Cache($pipeline, 'css');
 
 function expect($type, $actual)
 {
-	$expect = file_get_contents('_expect/expect.' . $type);
-	if ($expect === $actual)
-		return print("$type : Test passed                                                      <br>\n");
-	echo "ERROR : Expected <pre>$expect</pre>, got <pre>$actual</pre>";
+	$expect = str_replace("\r", '', trim(file_get_contents('_expect/expect.' . $type)));
+	$actual = str_replace("\r", '', trim($actual));
+	if ($expect == $actual)
+		echo "$type : Test passed.<br/>";
+	else
+		echo "ERROR -- $type
+<table>
+	<thead>
+		<tr>
+			<th>Expected</th>
+			<th>Got</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td width='50%'><pre>$expect</pre></td>
+			<td width='50%'><pre>$actual</pre></td>
+		</tr>
+	</tbody>
+</table>
+	Files are available in <pre>_results/<b>expect or got</b>.$type</pre>";
+	if (!file_exists('_results'))
+		mkdir('_results');
+	file_put_contents('_results/expect.'.$type, $expect);
+	file_put_contents('_results/got.'.$type, $actual);
 }
 expect('js', $js->getContent());
 expect('css', $css->getContent());
--- test/lib/assets/javascripts/jquery/jquery.js
+++ test/lib/assets/javascripts/jquery/jquery.js
@@ -1,1 +1,4 @@
+/*
+ *= require ./empty
+ */
 jQuery();
