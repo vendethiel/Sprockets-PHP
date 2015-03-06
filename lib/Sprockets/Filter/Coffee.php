a8b44243e97865af45e8a705218f64be5321f5f3
vendethiel <vendethiel@hotmail.fr>
2015-03-06 23:01:50 +0100

    fix #35 (and obv update tests accordinly)

--- lib/Sprockets/Filter/Coffee.php
+++ lib/Sprockets/Filter/Coffee.php
@@ -6,23 +6,30 @@ namespace Sprockets\Filter;
  */
 class Coffee extends Base
 {
+	protected $bare = false;
+
 	public function __invoke($content, $file, $dir, $vars)
 	{
-		$cache_file = $this->getCacheDir('coffee_' . md5($content) . '|js', __CLASS__);
+		$cache_file = $this->getCacheDir($file . '_' . $md5 = md5($content) . '|ls', __CLASS__);
 
-		$this->registerFile($file, $cache_file);
-
-		if (file_exists($cache_file))
-			return file_get_contents($cache_file);
-
-		$previous_error_reporting = error_reporting();
-		error_reporting(E_ERROR);
-
-		$script = \CoffeeScript\Compiler::compile($content, array('file' => $file));
-		file_put_contents($cache_file, $script);
-
-		error_reporting($previous_error_reporting);
+		$this->registerFile($file, $js_cache_file = str_replace('.ls', '.js', $cache_file));
 		
-		return $script;
+		if (file_exists($js_cache_file))
+			return file_get_contents($js_cache_file);
+
+		if (!file_exists($cache_file))
+			file_put_contents($cache_file, $content);
+
+		$opts = array('coffee-script/bin/coffee', '-c' . ($this->bare ? 'b' : ''), $cache_file);
+
+		$out = $this->processNode($opts);
+
+		if (!file_exists($js_cache_file))
+		{
+			throw new Exception\Filter("CoffeeScript Compilation Error<pre>" .
+			 str_replace($cache_file, $file, $out) . "</pre>");
+		}
+
+		return file_get_contents($js_cache_file);
 	}
 }
